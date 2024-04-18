2ed469fd6da0b33ef2c17fdc0af6b13bb4266538
Sura3607 <nguyenminhnhut3607@gmail.com>
2024-04-18 20:58:53 +0700

    LengthOfLastWord

---
+++ LeetCode/LengthOfLastWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class LengthOfLastWord
+    {
+        public int LengthofLastWord(string s)
+        {       
+            int cout = 0;
+            for (int i = s.Length -1; i >= 0; i--)
+            {
+                if (s[i] == ' ' && cout == 0) continue;
+                if (s[i] == ' ') return cout;
+                cout++;
+            }
+            return s.Length;
+        }
+    }
+}
